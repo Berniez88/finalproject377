479475ce30b13aba9cd308cd2b28810cefac69f0
Arthur <37461679+warthur123@users.noreply.github.com>
2020-12-03 16:34:52 -0500

    edited fetch

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,39 +1,15 @@
 const grade_letters = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-']
 let classbase = [];
+let profgradesbase = [];
 
 
 // process submit search form
 const form = document.getElementById('class-form');
 form.onsubmit = function (e) {
-
-
-    // fetch data from jsons loaded in server.js
-    fetch('/api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((fromServer) => fromServer.json())
-        .then((jsonFromServer) => classbase.push(...jsonFromServer))
-        .catch((err) => {
-            console.log(err);
-        });
-
-    // console.log(classbase);
-
-
-
-
-    // create classbase by joining data from both API jsons  (╯°□°)╯︵ ┻━┻
-
-
-
-
-
-
-
     e.preventDefault();
+
+
+
     console.log('dept:', form.department.value);
     console.log('course:', form.course.value);
     console.log('sec:', form.section.value);
@@ -42,6 +18,43 @@ form.onsubmit = function (e) {
     // consolidate form data into one array, remove empty data
     const formData = [form.department.value, form.course.value, form.section.value, form.semester.value].filter(e => e !== "");
     console.log('form data array pls wrk', formData);
+
+
+
+
+    // Get data for class grades
+    fetch('/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((fromServer) => fromServer.json())
+        .then((jsonFromServer) => profgradesbase.push(...jsonFromServer))
+        .catch((err) => {
+            console.log(err);
+        });
+
+
+    // Get data for classes
+    getData();
+
+    async function getData() {
+        const response = await fetch("https://raw.githubusercontent.com/umdio/umdio-data/master/courses/data/202008.json");
+        const data = await response.json();
+        classbase.push(...data);
+    }
+
+    /*async function getProfessorGradesData() {
+        const response = await fetch("https://api.planetterp.com/v1/grades?course=".concat(course));
+        const data = await response.json();
+        classbase.push(...data);
+    }*/
+
+
+    console.log(profgradesbase);
+
+
 
 
 
