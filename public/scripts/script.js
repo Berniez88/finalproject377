e26318b6aaaf5adb87905662134b427f0dad5195
Arthur <37461679+warthur123@users.noreply.github.com>
2020-12-04 20:57:01 -0500

    updated script pop up source details

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,16 +4,16 @@ let profgradesbase = [];
 function gpa_calc() {
     let gpa = 0;
     let num_c = 0;
-    const gradesobj = {'A+': 4, 'A' : 4, 'A-' : 3.7, 'B+' : 3.3, 'B': 3, 'B-' : 2.7, 'C+' : 2.3, 'C' : 2, 'C-' : 1.7, 'D+' : 1.3, 'D' : 1, 'D-' : 0.7, 'F' : 0};
+    const gradesobj = { 'A+': 4, 'A': 4, 'A-': 3.7, 'B+': 3.3, 'B': 3, 'B-': 2.7, 'C+': 2.3, 'C': 2, 'C-': 1.7, 'D+': 1.3, 'D': 1, 'D-': 0.7, 'F': 0 };
     for (const object in grades_array) {
-        for (const grade in object){
-            if (grade in object){
+        for (const grade in object) {
+            if (grade in object) {
                 gpa += (gradesobj[grade] * object[grade]);
                 num_c += (1 * object[grade]);
             };
         };
     };
-    return gpa/num_c;
+    return gpa / num_c;
 }
 
 // Fetch class data from UMD.io into classbase
@@ -77,11 +77,10 @@ classbase.push(...data);
     function findMatches(formData) {
         let matchList = [];
         // match dept
-        if (formData.department_input !== '') {
-            matchList = classbase.filter(curr => {
-                return curr.dept_id === formData.department_input;
-            });
-        }
+        matchList = classbase.filter(curr => {
+            return curr.dept_id === formData.department_input;
+        });
+
         // match course no
         if (formData.course_no_input !== '') {
             matchList = matchList.filter(curr => {
@@ -91,8 +90,6 @@ classbase.push(...data);
 
         console.log(matchList);
         return matchList;
-
-
 
     }
 
@@ -105,18 +102,43 @@ classbase.push(...data);
 
     // for each class in list of matching classes, generate html in results page.
     $('.search-results').empty();
-
     const matchList = findMatches(formData);
-
     const html = matchList.map(curr => {
 
         return `
         <div class="card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">${curr.course_id}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${curr.semester}</h6>
-          <p class="card-text"> ${curr.description}<br></p>
-          <a href="#" class="card-link">Card link</a>
+            <h5 class="card-title">${curr.course_id}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${curr.semester} Fall 2020</h6>
+            <p class="card-text">${curr.description}<br></p>
+            
+          
+          
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal${curr.course_id}">
+                Details
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal${curr.course_id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${curr.course_id} - ${curr.name} </h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <span><strong>Description:</strong></span>
+                            <p>${curr.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         
+
+          
         </div>
       </div>
         `;
