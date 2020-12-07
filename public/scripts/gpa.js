b119d24b7eadd87c0aeed2f554f430815c27bed8
jhersh4 <70652247+jhersh4@users.noreply.github.com>
2020-12-07 11:22:03 -0500

    :no_pedestrians:

--- public/scripts/gpa.js
+++ public/scripts/gpa.js
@@ -1,6 +1,6 @@
 let profgradesbase = [];
-let courseAvg = 4.0;
-let professorAvgs = {"Alex": 4.0, }
+let courseAvg = 0;
+let professorAvgs = {};
 
 // fetch data from api. api -> json -> array
 fetch('/api', {
@@ -17,23 +17,30 @@ fetch('/api', {
 
 console.log(profgradesbase);
 
-
-
 // gpa calculate
 function gpa_calc() {
-    let gpa = 0;
+    console.log("1");
     let num_c = 0;
     const gradesobj = { 'A+': 4, 'A': 4, 'A-': 3.7, 'B+': 3.3, 'B': 3, 'B-': 2.7, 'C+': 2.3, 'C': 2, 'C-': 1.7, 'D+': 1.3, 'D': 1, 'D-': 0.7, 'F': 0 };
-    for (const object in grades_array) {
-        for (const grade in object) {
-            if (grade in object) {
-                gpa += (gradesobj[grade] * object[grade]);
+    console.log(profgradesbase);
+    for (var index = 0; index < profgradesbase.length; index++) {
+    /*profgradesbase.forEach((element) => {
+      console.log("2");
+    });*/
+      console.log("2");
+  };
+         /*for (const grade in profgradesbase[index]) {
+          console.log("3")
+            if (grade in gradesobj) {
+                courseAvg += (gradesobj[grade] * object[grade]);
                 num_c += (1 * object[grade]);
+                console.log("4");
             };
-        };
+        };*/
     };
-    return gpa / num_c;
-}
+    // return courseAvg / num_c;
+
+const gpa = gpa_calc()
 
 
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,3 @@
-import './gpa.js';
 
 let classbase = [];
 
