3651692fb402ff9a1748600b75a188404474cd69
jhersh4 <70652247+jhersh4@users.noreply.github.com>
2020-12-07 17:01:27 -0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,19 +3,19 @@
 // generate html for avg grades and add html to page for each class
 // return: no return
 function calcCourseAvgs(profgradesbase) {
-    profgradesbase.forEach(course => {
-        console.log("if this doesnt work (╯°□°)╯︵ ┻━┻");
-    });
-
-
-
-
-
-
-}
-
-
-
+        let gpa = 0;
+        let num_c = 0;
+        const gradesobj = { 'A+': 4, 'A': 4, 'A-': 3.7, 'B+': 3.3, 'B': 3, 'B-': 2.7, 'C+': 2.3, 'C': 2, 'C-': 1.7, 'D+': 1.3, 'D': 1, 'D-': 0.7, 'F': 0 };
+        for (const object in grades_array) {
+            for (const grade in object) {
+                if (grade in object) {
+                  gpa += (gradesobj[grade] * object[grade]);
+                  num_c += (1 * object[grade]);
+                };
+              };
+            };
+            return gpa / num_c;
+          };
 
 // Get average GPA of the course
 async function getGPA(course) {
@@ -35,7 +35,7 @@ async function getGPA(course) {
         const profgradesbase = await data.json();
         console.log(profgradesbase);
 
-        calcCourseAvgs(profgradesbase);
+        console.log(calcCourseAvgs(profgradesbase));
 
 
         return profgradesbase;
